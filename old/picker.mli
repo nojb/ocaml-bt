a5b7c6aff39be8735f86070fd199896f06558358
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2014-04-16 22:47:15 +0100

    Delete [old/]

--- old/picker.mli
+++
@@ -1,9 +0,0 @@
-type t
-
-val create : int -> t
-val got_have : t -> int -> unit
-val lost_have : t -> int -> unit
-val did_request : t -> int -> unit
-val got_piece : t -> int -> unit
-val next : t -> (int -> bool) -> int option
-val is_complete: t -> bool
--- old/server.mli
+++
@@ -1,5 +0,0 @@
-type t
-
-val create : (Lwt_unix.file_descr * Unix.sockaddr -> unit) -> t
-val stop : t -> unit
-val port : t -> int
