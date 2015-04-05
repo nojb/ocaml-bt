11c59ad464f66fcfd2697eed97f7f6a2d6dad6fd
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2015-04-05 01:39:26 +0100

    Remove Torrent & Requester

--- lib/client.ml
+++ lib/client.ml
@@ -224,263 +224,37 @@ let rechoke peers =
   in
   loop None rechoke_optimistic_duration
 
-(* Requester BEGIN *)
-
-(* type active_block = { *)
-(*   a_piece : int; *)
-(*   a_block : int; *)
-(*   a_peer : Peer.t; *)
-(*   a_sent_at : float *)
-(* } *)
-
-(* type pending_piece = { *)
-(*   p_index : int; *)
-(*   mutable p_reqs : int; *)
-(*   p_salt : int; *)
-(*   mutable p_complete : bool *)
-(* } *)
-
-(* type t = { *)
-(*   meta : Metadata.t; *)
-(*   tor : Torrent.t; *)
-(*   rarity : int array; *)
-(*   pending : pending_piece array; *)
-(*   mutable active : active_block list *)
-(* } *)
-
-(* let compare_by_weight r p1 p2 = *)
-(*   let w p = *)
-(*     let m = Torrent.missing_blocks_in_piece r.tor p.p_index in *)
-(*     let reqs = p.p_reqs in *)
-(*     let a = if m > reqs then m - reqs else Metadata.block_count r.meta p.p_index + reqs in *)
-(*     (a, r.rarity.(p.p_index)) *)
-(*   in *)
-(*   match p1.p_complete, p2.p_complete with *)
-(*   | true, true -> 0 *)
-(*   | true, false -> 1 *)
-(*   | false, true -> -1 *)
-(*   | false, false -> *)
-(*     let (a1, h1) = w p1 in *)
-(*     let (a2, h2) = w p2 in *)
-(*     if a2 < a1 then 1 else *)
-(*     if a1 < a2 then -1 else *)
-(*     if h1 < h2 then -1 else *)
-(*     if h2 < h1 then 1 else *)
-(*     if p1.p_salt < p2.p_salt then 1 else *)
-(*     if p2.p_salt < p1.p_salt then -1 *)
-(*     else 0 *)
-
-(* let get_next_requests t peer n = *)
-(*   Array.sort (compare_by_weight t) t.pending; *)
-(*   let rec loop acc i = *)
-(*     if i >= Array.length t.pending || List.length acc >= n then List.rev acc *)
-(*     else *)
-(*       let p = t.pending.(i) in *)
-(*       if p.p_complete then List.rev acc *)
-(*       else *)
-(*       if not (Peer.has_piece peer p.p_index) then loop acc (i+1) *)
-(*       else *)
-(*         let rec loop' acc j = *)
-(*           if List.length acc >= n then List.rev acc else *)
-(*           if j >= Metadata.block_count t.meta p.p_index then loop acc (i+1) else *)
-(*           if List.exists (fun r -> r.a_piece = p.p_index && r.a_block = j) t.active then loop' acc (j+1) else *)
-(*           if not (Torrent.has_block t.tor p.p_index j) then loop' ((p, j) :: acc) (j+1) *)
-(*           else loop' acc (j+1) *)
-(*         in *)
-(*         loop' acc 0 *)
-(*   in *)
-(*   let reqs = loop [] 0 in *)
-(*   List.iter (fun (p, j) -> *)
-(*       t.active <- {a_piece = p.p_index; a_block = j; a_peer = peer; a_sent_at = Unix.time ()} :: t.active; *)
-(*       p.p_reqs <- p.p_reqs + 1) reqs; *)
-(*   List.map (fun (p, j) -> p.p_index, j) reqs *)
-
-(* let lookup t i = *)
-(*   let rec loop j = *)
-(*     if j >= Array.length t.pending then raise Not_found *)
-(*     else if t.pending.(j).p_index = i then t.pending.(j) else loop (j+1) *)
-(*   in *)
-(*   loop 0 *)
-
-(* let decrease_request_count t i = *)
-(*   let p = lookup t i in *)
-(*   p.p_reqs <- p.p_reqs - 1 *)
-
-(* let request_ttl_secs = 90 *)
-
-(* let refill_upkeep_period_msec = 10.0 *)
-
-(* let rec upkeep_pulse t = *)
-(*   let now = Unix.time () in *)
-(*   let too_old = now -. float request_ttl_secs in *)
-(*   let old, keep = List.partition (fun r -> r.a_sent_at <= too_old) t.active in *)
-(*   t.active <- keep; *)
-(*   List.iter (fun r -> *)
-(*       (\* debug "cancelling %d(%d/%d) because request is too old" *\) *)
-(*         (\* r.a_piece r.a_block (Metadata.block_count t.meta r.a_piece); *\) *)
-(*       Peer.send_cancel r.a_peer (r.a_piece, r.a_block); *)
-(*       decrease_request_count t r.a_piece) old; *)
-(*   Lwt_unix.sleep refill_upkeep_period_msec >>= fun () -> *)
-(*   upkeep_pulse t *)
-
-(* let peer_declined_all_requests tor peer = *)
-(*   List.iter (fun r -> if r.a_peer == peer then decrease_request_count tor r.a_piece) tor.active; *)
-(*   tor.active <- List.filter (fun r -> r.a_peer != peer) tor.active *)
-
-(* let got_block tor peer i b = *)
-(*   List.iter begin fun r -> *)
-(*     if r.a_piece = i && r.a_block = b then begin *)
-(*       decrease_request_count tor i; *)
-(*       if r.a_peer != peer then Peer.send_cancel r.a_peer (i, b) *)
-(*     end *)
-(*   end tor.active; *)
-(*   tor.active <- List.filter (fun r -> r.a_piece <> i || r.a_block <> b) tor.active *)
-
-(* let got_have self piece = *)
-(*   self.rarity.(piece) <- self.rarity.(piece) + 1 *)
-
-(* let lost_have self piece = *)
-(*   self.rarity.(piece) <- self.rarity.(piece) - 1 *)
-
-(* let got_piece r i = *)
-(*   let p = lookup r i in *)
-(*   p.p_complete <- true *)
-
-(* let got_bad_piece r i = *)
-(*   (\* FIXME *\) *)
-(*   () *)
-
-(* let create m t = *)
-(*   let numpieces = Metadata.piece_count m in *)
-(*   let create_pending_piece i = *)
-(*     { p_index = i; p_reqs = 0; p_salt = Random.bits (); p_complete = Torrent.has_piece t i } *)
-(*   in *)
-(*   let r = *)
-(*     { meta = m; tor = t; *)
-(*       pending = Array.init numpieces create_pending_piece; active = []; *)
-(*       rarity = Array.create numpieces 0 } *)
-(*   in *)
-(*   Lwt.async (fun () -> upkeep_pulse r); *)
-(*   r *)
-
 (* Requester END *)
 
-(* Torrent BEGIN *)
-
-module Torrent = struct
-
-  type t =
-    { meta : Metadata.t;
-      store : Store.t;
-      completed : Bits.t array;
-      mutable amount_left : int64;
-      handle : event -> unit }
-
-  let create meta handle =
-    let numpieces = Metadata.piece_count meta in
-    Store.create (Metadata.files meta) >>= fun store ->
-    let dl =
-      { meta; store;
-        completed = Array.init numpieces (fun i -> Bits.create @@ Metadata.block_count meta i);
-        amount_left = Metadata.total_length meta;
-        handle }
-    in
-    let plen = Metadata.piece_length dl.meta in
-    let rec loop good acc i =
-      if i >= numpieces then
-        Lwt.return (good, acc)
-      else
-        let off = Metadata.offset dl.meta i 0 in
-        Store.digest dl.store off (plen i) >>= fun digest ->
-        if SHA1.equal digest (Metadata.hash dl.meta i) then begin
-          Bits.set_all dl.completed.(i);
-          loop (good + 1) Int64.(sub acc (of_int (plen i))) (i + 1)
-        end else
-          loop good acc (i+1)
-    in
-    (* let start_time = Unix.gettimeofday () in *)
-    loop 0 (Metadata.total_length dl.meta) 0 >>= fun (good, amount_left) ->
-    (* let end_time = Unix.gettimeofday () in *)
-    (* debug "loaded, %d/%d good pieces, %Ld bytes left, in %.0fs" *)
-    (* good numpieces amount_left (end_time -. start_time); *)
-    dl.amount_left <- amount_left;
-    Lwt.return dl
-
-  let get_block t i off len =
-    Store.read t.store (Metadata.offset t.meta i off) len
-
-  let is_complete self =
-    let rec loop i =
-      (i >= Array.length self.completed) || (Bits.has_all self.completed.(i) && loop (i + 1))
-    in
-    loop 0
-
-  let got_block t i off s =
-    (* t.down <- Int64.add t.down (Int64.of_int (String.length s)); *)
-    let b = off / block_size in
-    match Bits.is_set t.completed.(i) b with
-    | false ->
-        Bits.set t.completed.(i) b;
-        (* debug "got block %d:%d (%d remaining)" idx b *)
-        (* (Bits.length t.completed.(idx) - Bits.count t.completed.(idx)); *)
-        Store.write t.store (Metadata.offset t.meta i off) s >>= fun () ->
-        if Bits.has_all t.completed.(i) then begin
-          Store.read t.store (Metadata.offset t.meta i off)
-            (Metadata.piece_length t.meta i) >>= fun s ->
-          if SHA1.(equal (digest s) @@ Metadata.hash t.meta i) then begin
-            t.amount_left <- Int64.(sub t.amount_left (of_int (Metadata.piece_length t.meta i)));
-            t.handle (PieceVerified i);
-            if is_complete t then
-              Lwt.wrap1 t.handle TorrentComplete
-            else
-              Lwt.return_unit
-          end
-          else begin
-            Bits.clear t.completed.(i);
-            Lwt.wrap1 t.handle @@ PieceFailed i
-          end
-        end
-        else
-          Lwt.return_unit
-    | ture ->
-        (* debug "received a block we already have" *)
-        Lwt.return_unit
-
-  let got_block t i off s =
-    ignore
-      (Lwt.catch (fun () -> got_block t i off s)
-         (fun _ -> (* log warning *) Lwt.return_unit))
-
-  let amount_left self =
-    self.amount_left
-
-  let numgot self =
-    Array.fold_left (fun n b -> if Bits.has_all b then n+1 else n) 0 self.completed
-
-  let have self =
-    let have = Bits.create (Array.length self.completed) in
-    Array.iteri (fun i b -> if Bits.has_all b then Bits.set have i) self.completed;
-    have
-
-  let has_piece self i =
-    Bits.has_all self.completed.(i)
-
-  let has_block t i j _ =
-    Bits.is_set t.completed.(i) (j / block_size)
-
-  let missing_blocks_in_piece t i =
-    Bits.count_zeroes t.completed.(i)
-
-  let have_size t =
-    let n = ref 0L in
-    for i = 0 to Array.length t.completed - 1 do
-      if Bits.has_all t.completed.(i) then
-        n := Int64.add !n (Int64.of_int (Metadata.piece_length t.meta i))
-    done;
-    !n
-
-end
-(* Torrent END *)
+  (* let create meta handle = *)
+  (*   let numpieces = Metadata.piece_count meta in *)
+  (*   Store.create (Metadata.files meta) >>= fun store -> *)
+  (*   let dl = *)
+  (*     { meta; store; *)
+  (*       completed = Array.init numpieces (fun i -> Bits.create @@ Metadata.block_count meta i); *)
+  (*       amount_left = Metadata.total_length meta; *)
+  (*       handle } *)
+  (*   in *)
+  (*   let plen = Metadata.piece_length dl.meta in *)
+  (*   let rec loop good acc i = *)
+  (*     if i >= numpieces then *)
+  (*       Lwt.return (good, acc) *)
+  (*     else *)
+  (*       let off = Metadata.offset dl.meta i 0 in *)
+  (*       Store.digest dl.store off (plen i) >>= fun digest -> *)
+  (*       if SHA1.equal digest (Metadata.hash dl.meta i) then begin *)
+  (*         Bits.set_all dl.completed.(i); *)
+  (*         loop (good + 1) Int64.(sub acc (of_int (plen i))) (i + 1) *)
+  (*       end else *)
+  (*         loop good acc (i+1) *)
+  (*   in *)
+  (*   (\* let start_time = Unix.gettimeofday () in *\) *)
+  (*   loop 0 (Metadata.total_length dl.meta) 0 >>= fun (good, amount_left) -> *)
+  (*   (\* let end_time = Unix.gettimeofday () in *\) *)
+  (*   (\* debug "loaded, %d/%d good pieces, %Ld bytes left, in %.0fs" *\) *)
+  (*   (\* good numpieces amount_left (end_time -. start_time); *\) *)
+  (*   dl.amount_left <- amount_left; *)
+  (*   Lwt.return dl *)
 
 type piece_state =
   | Pending
@@ -697,7 +471,7 @@ let share_torrent bt meta store pieces peers =
 
     | PeerEvent (_, Peer.Interested)
     | PeerEvent (_, Peer.NotInterested) ->
-        (* if not (am_choking peers id) then Choker.rechoke ch; *)
+        (* rechoke *)
         loop ()
 
     | PeerEvent (id, Peer.MetaRequested i) ->
@@ -756,7 +530,8 @@ let share_torrent bt meta store pieces peers =
   loop ()
 
 let load_torrent bt meta =
-  Torrent.create meta bt.push (* >|= fun dl -> *)
+  assert false
+  (* Torrent.create meta bt.push (\* >|= fun dl -> *\) *)
 (* bt.push (TorrentLoaded dl) *)
 
 (* IncompleteMetadata BEGIN *)
