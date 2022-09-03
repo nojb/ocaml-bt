1d3f42e5d93b854c0ac384bedd8aa3884b3af4fa
nojebar <nicolas.ojeda.bar@lexifi.com>
2022-09-03 09:31:10 +0200

    _

--- bin/main.ml
+++ bin/main.ml
@@ -2,30 +2,37 @@ open Bt
 
 let spec = []
 
-let random state n =
+let read_torrent_file path =
+  let s = In_channel.with_open_bin path In_channel.input_all in
+  let t =
+    match Bencode.decode s with
+    | Some t -> t
+    | None -> failwith (Printf.sprintf "Could not parse torrent file %S" path)
+  in
+  match Bencode.Decoder.query Metainfo.decoder t with
+  | Some t -> t
+  | None ->
+      failwith (Printf.sprintf "Could not parse metainfo file %S" path)
+
+let random_string state n =
   String.init n (fun _ -> char_of_int (Random.State.int state 256))
 
-let anon ~env ~ran path =
-  let s = In_channel.with_open_bin path In_channel.input_all in
-  match Bencode.decode s with
-  | Some t -> (
-      match Bencode.Decoder.query Metainfo.decoder t with
-      | Some t ->
-          let info_hash = t.Metainfo.info_hash in
-          let peer_id = random ran 20 in
-          let port = Random.State.int ran 10000 in
-          let left = Metainfo.total_length t in
-          let resp =
-            Tracker.announce ~net:(Eio.Stdenv.net env) ~info_hash ~peer_id ~port
-              ~uploaded:0 ~downloaded:0 ~left t.Metainfo.announce
-          in
-          let sexp = Tracker.Response.to_sexp resp in
-          Format.printf "@[%a@]@." Sexp.print sexp
-      | None ->
-          failwith (Printf.sprintf "Could not parse metainfo file %S" path))
-  | None -> failwith (Printf.sprintf "Could not parse torrent file %S" path)
+let anon ~env ~rng path =
+  let t = read_torrent_file path in
+  let info_hash = t.Metainfo.info_hash in
+  let peer_id = random_string rng 20 in
+  let port = Random.State.int rng 10000 in
+  let left = Metainfo.total_length t in
+  let resp =
+    Tracker.announce ~net:(Eio.Stdenv.net env) ~info_hash ~peer_id ~port
+      ~uploaded:0 ~downloaded:0 ~left t.Metainfo.announce
+  in
+  let sexp = Tracker.Response.to_sexp resp in
+  Format.printf "@[%a@]@." Sexp.print sexp
+
+let anon path =
+  let rng = Random.State.make_self_init () in
+  Eio_main.run (fun env -> anon ~env ~rng path)
 
 let () =
-  Eio_main.run (fun env ->
-      let ran = Random.State.make_self_init () in
-      Arg.parse (Arg.align spec) (anon ~env ~ran) "")
+  Arg.parse (Arg.align spec) anon ""
